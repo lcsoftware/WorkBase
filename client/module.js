2e7ed9b8faa46fe43d930dfcb79abfcf5ae120ef
lcsoftware <lcsoftware@126.com>
2015-07-23 13:00:12 +0800

    auto

---
+++ client/module.js
@@ -0,0 +1,40 @@
+﻿
+'use strict';
+
+angular.module('commonModule', []);
+
+angular.module('componentModule', ['commonModule']);
+
+angular.module('authModule', []);
+
+angular.module('auditModule', []);
+
+angular.module('homeModule', []);
+
+angular.module('userModule', ['componentModule']);
+
+angular.module('partnerModule', []);
+
+angular.module('donationModule', []);
+
+angular.module('projectModule', []);
+
+angular.module('auditModule', []);
+
+angular.module('fileModule', []);
+
+angular.module('sysModule', []);
+
+angular.module('filterModule', [])
+
+angular.module('directiveModule', []);
+
+angular.module('hbUploaderModule', []);
+
+angular.module('app', [
+  'ui.router', 'ui.bootstrap', 'checklist-model', 'ngCookies',
+  'commonModule', 'componentModule', 'authModule', 'userModule',
+  'projectModule', 'auditModule', 'fileModule', 'directiveModule',
+  'homeModule', 'partnerModule', 'donationModule', 'sysModule',
+  'filterModule', 'hbUploaderModule', 'auditModule'
+]);
